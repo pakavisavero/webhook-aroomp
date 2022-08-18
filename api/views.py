eb791f59b15f08dd8ce0b1c5b66a314633876c3f
pakavisaveo <pakavisaveroo@gmail.com>
2022-08-18 20:30:38 +0700

    add allowed host

--- api/views.py
+++ api/views.py
@@ -1,9 +1,9 @@
 import json
-from django.views.decorators.http import require_http_methods
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.http import require_http_methods
+# from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
-@require_http_methods(["POST"])
+# @csrf_exempt
+# @require_http_methods(["POST", "GET"])
 def get_webhook(request):
     data = json.loads(request.body.decode('utf-8'))
     print(data)
